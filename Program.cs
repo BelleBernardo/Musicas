b58c75635e3993d7e4e7ae727f363638f3259dce
isabelle <e107ti@cps.sp.gov.br>
2022-03-04 22:40:11 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Halley's Comet");
+Console.ResetColor();
+Console.WriteLine("I don't want it");
+Console.WriteLine("And I don't want to want you");
+Console.WriteLine("But in my dreams I seem to be more honest");
+Console.WriteLine("And I must admit, you've been in quite a few");
+
+Console.WriteLine("Halley's Comet");
+Console.WriteLine("Comes around more than I do");
+Console.WriteLine("But you're all it takes for me to break a promise");
+Console.WriteLine("Silly me to fall in love with you");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I haven't slept since Sunday");
+Console.WriteLine("Midnight for me is 3:00 a.m. for you");
+Console.WriteLine("Midnight for me is 3:00 a.m. for you");
+Console.WriteLine("With you than nights could ever be alone, ooh-ooh-ooh");
+Console.WriteLine("I was good at feeling nothing, now I'm hopeless");
+Console.WriteLine("What a drag to love you like I do, ooh-ooh, ooh, ooh");
+Console.WriteLine("Ooh-ooh-ooh, ooh, ooh-ooh, ooh");
+
+Console.ResetColor();
+Console.WriteLine("I've been loved before, but right now in this moment");
+Console.WriteLine("I feel more and more like I was made for you");
+
+Console.WriteLine("For you");
+
+Console.WriteLine("I'm sitting in my brother's room");
+Console.WriteLine("Haven't slept in a week or two, or two");
+Console.WriteLine("I think I might have fallen in love");
+Console.WriteLine("What am I to do?");
